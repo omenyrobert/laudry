744acbff99fe2088fd313e38df0394dc57786f52
omenyrobert <macrobert000@gmail.com>
2023-07-15 20:02:26 +0300

    chanes

--- src/Database/database.ts
+++ src/Database/database.ts
@@ -2,11 +2,11 @@ import { DataSource } from "typeorm";
 import { join } from "path";
 
 export const DatabaseConnection = new DataSource({
-  host: "sql865.main-hosting.eu",
+  host: "localhost",
   type: "mysql",
-  username: "u848751863_school",
-  password: "@Jollyjoe123",
-  database: "u848751863_school",
+  username: "root",
+  password: "",
+  database: "school",
   logging: false,
   synchronize: true,
   entities: [join(__dirname, "..", "Entities/*.ts")],
