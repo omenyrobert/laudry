e209c2ea5cd55b3e2e85c630bfdb5180147611f8
Jim Junior <jimjunior854@gmail.com>
2023-07-22 19:24:47 +0300

    Create Desktop app and add activation token script

--- src/Helpers/Hash.ts
+++ src/Helpers/Hash.ts
@@ -17,6 +17,13 @@ export const encrypt = (date: string | null = null) => {
     const in2Days = now + 2 * 24 * 60 * 60 * 1000
     const newDate = new Date(in2Days)
     date = newDate.toISOString()
+  } else {
+    const dateRegex = new RegExp(/\d{4}-\d{2}-\d{2}/);
+    if (!dateRegex.test(date)) {
+      throw new Error("Invalid date format");
+    }
+    const newDate = new Date(date)
+    date = newDate.toISOString()
   }
 
   const cipher = crypto.createCipheriv(algorithm, secretKey, encryptionIV)
---
+++ src/token.ts
@@ -0,0 +1,34 @@
+import { decrypt, encrypt } from "./Helpers/Hash";
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+import chalk from "chalk"
+
+const log = console.log;
+
+async function generateHash() {
+  const rl = readline.createInterface({ input, output });
+  const question = `Enter date in of expiry for the token:
+  Format MUST be: ${chalk.underline("YYYY-MM-DD")}
+  >>>> `
+  const answer = await rl.question(question);
+
+  rl.close();
+
+  const dateRegex = new RegExp(/\d{4}-\d{2}-\d{2}/);
+  if (!dateRegex.test(answer)) {
+    console.log("Invalid date format");
+    return;
+  }
+
+  try {
+    const hash = encrypt(answer);
+    log(chalk.green("Token generated successfully!"))
+    log(`TOKEN: ${chalk.gray(hash)}`)
+  } catch (error) {
+    log(chalk.red("Error generating token"))
+  }
+}
+generateHash();
+
+
+
