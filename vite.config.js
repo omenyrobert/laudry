b1874ac4ae3c827d14138daaa6e2339097d2ddbc
Elian-ui <munelian @gmail.com>
2025-09-30 18:15:35 +0300

    updated vite file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "frontend/build", // 👈 build output folder
+    emptyOutDir: true, // clears folder before build
+  },
+});
